8c019c5851954d966c24281e0a9146965e42f598
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-08-15 19:09:55 +0200

    Copiata "prettyPrint" dalla pagina di TOP per  visualizzare l'albero sulla console; "buildTree" ora costruisce l'albero: al momento  lo lascio con eventuali numeri doppi;

--- src/index.js
+++ src/index.js
@@ -10,8 +10,25 @@ class Tree {
   constructor(arr) {
     this.arr = arr;
     this.sorted = this.mergeSort(arr);
-    this.root = this.buildTree(arr);
+    this.root = this.buildTree(this.sorted);
+    this.prettyPrint(this.root);
   }
+  prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
 
   mergeSort = (arr) => {
     let sorted = [];
@@ -41,10 +58,24 @@ class Tree {
   };
 
   buildTree = (arr) => {
-    return true;
+    console.log(arr);
+    let middle = Math.floor((arr.length - 1) / 2);
+    let tree = null;
+
+    if (arr.length === 1) {
+      return new Node(arr[middle], null, null);
+    } else if (arr.length === 0) {
+      return new Node(null, null, null);
+    } else {
+      let left = this.buildTree(arr.slice(0, middle));
+      let right = this.buildTree(arr.slice(middle + 1));
+      console.log("left", left, "right", right);
+      tree = new Node(arr[middle], left, right);
+      return tree;
+    }
   };
 }
 
-const arr = [2, 3, 1, 7, 6, 4, 5, 0];
+const arr = [2, 3, 1, 7, 6, 4, 5, 0, 10, 15, 26, 1];
 const test = new Tree(arr);
 console.log(test);
