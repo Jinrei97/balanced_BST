dec04bd5efeca68c37de245e6fb4de89f8237811
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-08-15 17:34:30 +0200

    Create classi "Node" e "Tree"; "Tree" ora usa mergeSort come metodo per ordinare  l'array su cui costruire il BST.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log("linked");
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    this.arr = arr;
+    this.sorted = this.mergeSort(arr);
+    this.root = this.buildTree(arr);
+  }
+
+  mergeSort = (arr) => {
+    let sorted = [];
+    if (arr.length === 1) {
+      return [arr[0]];
+    } else {
+      const left = this.mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
+      const right = this.mergeSort(arr.slice(Math.floor(arr.length / 2)));
+      sorted = this.mergeSortMergeHalves(left, right);
+      return sorted;
+    }
+  };
+  mergeSortMergeHalves = (left, right) => {
+    let sorted = [];
+    let i = 0;
+    while (i < 20 && (left.length > 0 || right.length > 0)) {
+      if (left.length > 0 && right.length > 0) {
+        sorted.push(left[0] < right[0] ? left.shift() : right.shift());
+      } else if (left.length === 0) {
+        sorted.push(right.shift());
+      } else {
+        sorted.push(left.shift());
+      }
+      i += 1;
+    }
+    return sorted;
+  };
+
+  buildTree = (arr) => {
+    return true;
+  };
+}
+
+const arr = [2, 3, 1, 7, 6, 4, 5, 0];
+const test = new Tree(arr);
+console.log(test);
